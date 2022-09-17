d30793bf83e427294c6be97948efe0c8399bfcfa
lena <sue822330@gmail.com>
2022-09-17 16:39:35 +0900

    feat: 네트워크 코드 추가
    - Alamofire를 사용해서 작업

--- LiveGodLife/LiveGodLife/ViewController.swift
+++ LiveGodLife/LiveGodLife/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let headers: HTTPHeaders = ["Content-Type": "application/json",
+                                    "Accept": "application/json"]
+        AF.request("http://192.168.33.60:8080/hello", method: .get, headers: headers)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let jsonData = JSON(value)
+                    print(jsonData)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
     }
-
-
 }
 
