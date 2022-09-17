0c68389a7e240d0380360d5ea256b13087a56d48
lena <sue822330@gmail.com>
2022-09-17 15:55:54 +0900

    Init Project

---
+++ LiveGodLife/LiveGodLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveGodLife
+//
+//  Created by Ador on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
