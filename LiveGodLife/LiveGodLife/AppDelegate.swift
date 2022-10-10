702e90a83f0151fe643d3b07b3bf137e9af3ffda
khahn <kwanghun0127@gmail.com>
2022-10-10 15:52:26 +0900

    feat: kakao 로그인, 닉네임, 회원기입 테스트
    # Conflicts:
    #	LiveGodLife/LiveGodLife/Info.plist
    #	LiveGodLife/LiveGodLife/ViewController.swift

--- LiveGodLife/LiveGodLife/AppDelegate.swift
+++ LiveGodLife/LiveGodLife/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "74934eaf8d7dfda42f617faa69616809")
         return true
     }
 
