1a5a214647e21fde58d6262a4bc9d9fb8c1aa893
lena <sue822330@gmail.com>
2022-10-22 12:17:54 +0900

    feat: 메인 스토리보드 삭제 및 온보딩 뷰 연결

---
+++ LiveGodLife/LiveGodLife/RootViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RootViewController.swift
+//  LiveGodLife
+//
+//  Created by Ador on 2022/09/17.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(forName: .moveToLogin, object: nil, queue: .main) { [weak self] _ in
+            let loginViewController = LoginViewController.instance()!
+            self?.present(loginViewController, animated: true)
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if isBeingPresented || isMovingToParent {
+            let onboardingViewController = OnboardingViewController.instance()!
+            onboardingViewController.modalPresentationStyle = .fullScreen
+            present(onboardingViewController, animated: true)
+        }
+    }
+}
+
