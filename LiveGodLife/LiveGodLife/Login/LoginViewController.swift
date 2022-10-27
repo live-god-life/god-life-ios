d9f29fb5c3221ab66d0335f91e87d0f925d528c1
lena <sue822330@gmail.com>
2022-10-27 21:30:26 +0900

    refactor: 로그인뷰 코드베이스로 리팩토링

--- LiveGodLife/LiveGodLife/Login/LoginViewController.swift
+++ LiveGodLife/LiveGodLife/Login/LoginViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 import AuthenticationServices
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
-    @IBOutlet weak var appleLoginButton: RoundedButton!
-    @IBOutlet weak var kakaoLoginButton: RoundedButton!
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    private let appleLoginButton = RoundedButton()
+    private let kakaoLoginButton = RoundedButton()
 
     private var appleLoginService: AppleLoginService?
 
@@ -22,26 +25,56 @@ class LoginViewController: UIViewController {
         setupUI()
     }
 
-    func setupUI() {
-        appleLoginButton.configure(title: "Apple로 시작하기")
-        kakaoLoginButton.configure(title: "카카오계정으로 시작하기")
+    private func setupUI() {
+        titleLabel.text = "갓생살기"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont(name: "Pretendard-Bold", size: 26)
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(79)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(34)
+        }
+        subtitleLabel.text = "꿈꾸는 갓생러들이 모인 곳"
+        subtitleLabel.textColor = .gray1
+        subtitleLabel.font = UIFont(name: "Pretendard", size: 16)
+        view.addSubview(subtitleLabel)
+        subtitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(34)
+        }
+
+        setupButtons()
     }
 
-    @IBAction func didTapAppleLoginButton(_ sender: Any) {
+    private func setupButtons() {
+        appleLoginButton.configure(title: "Apple로 시작하기", backgroundColor: .white)
+        appleLoginButton.addTarget(self, action: #selector(didTapAppleLoginButton), for: .touchUpInside)
+        kakaoLoginButton.configure(title: "카카오계정으로 시작하기", backgroundColor: UIColor(red: 247/255, green: 227/255, blue: 23/255, alpha: 1))
+        kakaoLoginButton.addTarget(self, action: #selector(didTapKaKaoLoginButton), for: .touchUpInside)
+
+        let buttonStackView = UIStackView()
+        buttonStackView.axis = .vertical
+        buttonStackView.spacing = 20
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.addArrangedSubview(appleLoginButton)
+        buttonStackView.addArrangedSubview(kakaoLoginButton)
+        view.addSubview(buttonStackView)
+        buttonStackView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(24)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(40)
+            $0.height.equalTo(132)
+        }
+    }
+
+    @objc private func didTapAppleLoginButton() {
         appleLoginService?.login()
     }
+
+    @objc private func didTapKaKaoLoginButton() { }
 }
 
 extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return view.window ?? ASPresentationAnchor()
-    }
-}
-
-extension UIViewController {
-
-    static func instance() -> Self? {
-        let name = String(describing: self)
-        return UIStoryboard(name: name, bundle: nil).instantiateViewController(withIdentifier: name) as? Self
     }
 }
--- LiveGodLife/LiveGodLife/UI/UIColor+Extension.swift
+++ LiveGodLife/LiveGodLife/UI/UIColor+Extension.swift
@@ -9,5 +9,8 @@ import UIKit
 
 extension UIColor {
 
-    static let green = UIColor(red: 124 / 255, green: 1, blue: 0, alpha: 1)
+    /// #7CFC00
+    static let green = UIColor(red: 124/255, green: 1, blue: 0, alpha: 1)
+    /// #CCCCCC
+    static let gray1 = UIColor(red: 204/255, green: 204/255, blue: 204/255, alpha: 1)
 }
