5a648c4869ab8b91cece0c79a31edab22e324f68
lena <sue822330@gmail.com>
2022-11-25 23:13:04 +0900

    feat: hoem todo cell 디데이 표시 추가

--- LiveGodLife/LiveGodLife/Home/Cell/TodoCollectionViewCell.swift
+++ LiveGodLife/LiveGodLife/Home/Cell/TodoCollectionViewCell.swift
@@ -11,23 +11,33 @@ final class TodoCollectionViewCell: UICollectionViewCell {
 
     static let identifier = "TodoCollectionViewCell"
 
+    @IBOutlet weak var dDayLabel: UILabel!
     @IBOutlet weak var repetitionLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var checkButton: UIButton!
+
+    private var id: Int?
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
         layer.cornerRadius = 51
-        checkButton.tintColor = .green
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1).cgColor
+        contentView.backgroundColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
         checkButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
-        checkButton.setImage(UIImage(systemName: "checkmark.circle"), for: .selected)
-        contentView.backgroundColor = UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)
     }
 
     func configure(_ todo: Todo.Schedule) {
+        id = todo.scheduleID
+        let dDayText = todo.dDay == 0 ? "D-Day" : "D-\(todo.dDay)"
+        dDayLabel.text = dDayText
         repetitionLabel.text = todo.repetitions.joined(separator: ",")
         repetitionLabel.isHidden = todo.repetitions.isEmpty
         contentLabel.text = todo.title
+    }
+
+    @IBAction func didTapCheckButton(_ sneder: UIButton) {
+
     }
 }
