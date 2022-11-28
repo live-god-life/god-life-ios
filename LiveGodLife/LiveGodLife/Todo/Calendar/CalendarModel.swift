59bc7712c4c0d01f93d337a951ebd7a3237519e0
kwanghun <kh.ahn@quintet.co.kr>
2022-11-28 12:32:15 +0900

    fix - #18 코드 리뷰 요청 처리

--- LiveGodLife/LiveGodLife/Todo/Calendar/CalendarModel.swift
+++ LiveGodLife/LiveGodLife/Todo/Calendar/CalendarModel.swift
@@ -7,66 +7,6 @@
 
 import Foundation
 
-//"data": [
-//        {
-//            "goalId": 1,
-//            "title": "이직하기",
-//            "todoSchedules": [
-//                {
-//                    "todoScheduleId": 43,
-//                    "todoId": 2,
-//                    "title": "컨셉잡기",
-//                    "completionStatus": false,
-//                    "taskType": "Todo",
-//                    "repetitionType": "DAY",
-//                    "repetitionParams": null,
-//                    "totalTodoTaskScheduleCount": 91,
-//                    "completedTodoTaskScheduleCount": 0,
-//                    "todoDay": -48
-//                }
-//            ]
-//        },
-
-//
-/// samplCode Data
-/// 해당 샘플 데이터는 삭제예정
-/*
-{
-            "goalId": 2,
-            "title": "이직하기",
-            "todoSchedules": [
-                {
-                    "todoScheduleId": 157,
-                    "todoId": 10,
-                    "title": "컨셉잡기",
-                    "completionStatus": false,
-                    "taskType": "Todo",
-                    "repetitionType": "DAY",
-                    "repetitionParams": null,
-                    "totalTodoTaskScheduleCount": 91,
-                    "completedTodoTaskScheduleCount": 0,
-                    "todoDay": -48
-                },
-                {
-                    "todoScheduleId": 224,
-                    "todoId": 11,
-                    "title": "스케치",
-                    "completionStatus": false,
-                    "taskType": "Todo",
-                    "repetitionType": "WEEK",
-                    "repetitionParams": [
-                        "월",
-                        "목",
-                        "토"
-                    ],
-                    "totalTodoTaskScheduleCount": 39,
-                    "completedTodoTaskScheduleCount": 0,
-                    "todoDay": -48
-                }
-            ]
-        }
-*/
-
 struct MainGoals:Codable {
     var title:String
     var goalId:Int
