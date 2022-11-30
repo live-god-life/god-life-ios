5c6426e8566e9dfe8c046566569b1e89f1da0beb
lena <sue822330@gmail.com>
2022-11-30 22:41:59 +0900

    feat: 마인드셋 최신 작업 추가
    빌드 에러 수정
    - 누락된 파일 추가
      - UIView+Extension
      - UIImage+Extension
    - 중복 이미지 삭제: left_quate, right_quate

--- LiveGodLife/LiveGodLife/Extension/UIViewController+Extension.swift
+++ LiveGodLife/LiveGodLife/Extension/UIViewController+Extension.swift
@@ -8,11 +8,8 @@
 import UIKit
 
 extension UIViewController {
-
     static func instance() -> Self? {
         let name = String(describing: self)
         return UIStoryboard(name: name, bundle: nil).instantiateViewController(withIdentifier: name) as? Self
     }
-    
-
 }
