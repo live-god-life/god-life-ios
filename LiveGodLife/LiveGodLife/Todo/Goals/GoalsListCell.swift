777764b1eed69db88c6672f55333a255de893118
khAhn127 <83949305+khAhn127@users.noreply.github.com>
2022-12-06 07:22:41 +0900

    마인드셋 이미지 파일 명 수정, 목표탭 디자인 추가

--- LiveGodLife/LiveGodLife/Todo/Goals/GoalsListCell.swift
+++ LiveGodLife/LiveGodLife/Todo/Goals/GoalsListCell.swift
@@ -28,28 +28,136 @@ class GoalsListCell: CommonCell {
         let label = UILabel()
         return label
     }()
+    let progress = UIProgressView()
+
+    lazy var contentsView:UIView = {
+        let view = UIView()
+        
+        self.datelabel.textColor = .white
+        self.mindsetCountLabel.textColor = .white
+        self.totalTodoCountLabel.textColor = .white
+        self.completedTodoCount.textColor = .white
+        self.datelabel.textColor = .white
+        
+        self.mindsetImage.image = UIImage(named: "mindset")
+        self.totalTodoImage.image = UIImage(named: "proceeding")
+        self.completedTodoImage.image = UIImage(named: "complete")
+        
+        
+        view.addSubview(self.titleLabel)
+        view.addSubview(self.dDayLabel)
+        
+        view.addSubview(self.mindsetImage)
+        view.addSubview(self.mindsetCountLabel)
+        view.addSubview(self.totalTodoImage)
+        view.addSubview(self.totalTodoCountLabel)
+        view.addSubview(self.completedTodoImage)
+        view.addSubview(self.completedTodoCount)
+        
+        view.addSubview(self.progress)
+
+        view.addSubview(self.datelabel)
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalTo(self.dDayLabel.snp.left)
+            $0.height.equalTo(30)
+        }
+        self.dDayLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(-16)
+            $0.height.equalTo(30)
+
+        }
+        
+        self.mindsetImage.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.equalTo(16)
+            $0.height.equalTo(16)
+        }
+        self.mindsetCountLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(self.mindsetImage.snp.right).offset(3)
+        }
+        self.totalTodoImage.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(self.mindsetCountLabel.snp.right).offset(12)
+            $0.width.equalTo(16)
+            $0.height.equalTo(16)
+        }
+        self.totalTodoCountLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(self.totalTodoImage.snp.right).offset(3)
+        }
+        self.completedTodoImage.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(self.totalTodoCountLabel.snp.right).offset(12)
+            $0.width.equalTo(16)
+            $0.height.equalTo(16)
+        }
+        self.completedTodoCount.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            $0.left.equalTo(self.completedTodoImage.snp.right).offset(3)
+        }
+        
+        self.progress.snp.makeConstraints {
+            $0.top.equalTo(self.mindsetImage.snp.bottom).offset(16)
+            $0.left.equalToSuperview().offset(16)
+            $0.right.equalToSuperview().offset(16)
+        }
+        
+        self.datelabel.snp.makeConstraints {
+            $0.top.equalTo(self.progress.snp.bottom).offset(8)
+            $0.left.equalToSuperview().offset(16)
+            $0.bottom.equalToSuperview().offset(-16)
+        }
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.layer.cornerRadius = 20
+        self.layer.borderColor = UIColor.darkGray.cgColor
+        self.layer.borderWidth = 1
+        
+        
         addViews()
+        self.update()
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         fatalError("init(coder:) has not been implemented")
     }
 
-    func addViews(){
-        addSubview(titleLabel)
-        titleLabel.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.left.right.equalToSuperview()
+    func addViews() {
+        self.addSubview(self.contentsView)
+        self.contentsView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview().offset(5)
+            make.bottom.equalToSuperview().offset(-5)
         }
-        
-        self.backgroundColor = .blue
     }
 
     func update() {
-        titleLabel.text = dataModel?.title ?? ""
+        titleLabel.text = dataModel?.title ?? "데일리 코딩"
+        titleLabel.textColor = .white
+        dDayLabel.textColor = .green
+        
+        let minsetCount = self.dataModel?.totalMindsetCount ?? 0
+        mindsetCountLabel.text = "마인드셋:\(minsetCount)"
+        let ProceedingCount = self.dataModel?.totalMindsetCount ?? 0
+        totalTodoCountLabel.text = "진행중:\(ProceedingCount)"
+        let completeCount = self.dataModel?.totalMindsetCount ?? 0
+        completedTodoCount.text = "완료:\(completeCount)"
+        dDayLabel.text = "D-24"
+        let startDate = self.dataModel?.startDate ?? (Date.today.date?.toString() ?? "")
+        let endDate = self.dataModel?.endDate ?? (Date.today.date?.toString() ?? "")
+        datelabel.text = "\(startDate) ~ \(endDate)"
+        
+//        dDayLabel.text = self.dataModel?.dDay ??"D-24"
     }
     
     override func setUpModel() {
