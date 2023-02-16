d68b48c5bce3cfc35e72fcd08337b6697cf6ea39
wargi <wkahdla12346@gmail.com>
2023-02-16 22:09:41 +0900

    feature: 달력 반복 주기 구현(3)

--- LiveGodLife/LiveGodLife/Scene/Todo/Create/Popuo/RepeatPopupVC.swift
+++ LiveGodLife/LiveGodLife/Scene/Todo/Create/Popuo/RepeatPopupVC.swift
@@ -1,5 +1,5 @@
 //
-//  RepeatVC.swift
+//  RepeatPopupVC.swift
 //  LiveGodLife
 //
 //  Created by wargi on 2023/02/26.
@@ -10,22 +10,31 @@ import SnapKit
 import UIKit
 import Combine
 
-protocol RepeatVCDelegate: AnyObject {
-    func select(startDate: Date?, endDate: Date?)
+protocol RepeatPopupVCDelegate: AnyObject {
+    func select(days: Set<Int>)
 }
 
 //MARK: DatePopupVC
-final class RepeatVC: UIViewController {
+final class RepeatPopupVC: UIViewController {
+    enum RepeatType {
+        case everyday
+        case weekday
+        case weekend
+        case custom(Set<Int>)
+    }
     //MARK: - Properties
-    weak var delegate: CalendarPopupVCDelegate?
     private var startDate: Date?
     private var endDate: Date?
+    weak var delegate: RepeatPopupVCDelegate?
     private var bag = Set<AnyCancellable>()
-    private let years = (2000 ... 2100).map { $0 }
-    private let month = (1 ... 12).map { $0 }
+    @Published private var repeatType: RepeatType = .everyday
+    private var days = Set<Int>()
     private let calendar = Calendar.current
     private let visualEffectView = CustomVisualEffectView()
-    private let containerView = UIView().then {
+    private let containerView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 16.0
+        $0.distribution = .fill
         $0.backgroundColor = .black
         $0.layer.cornerRadius = 24.0
     }
@@ -33,31 +42,51 @@ final class RepeatVC: UIViewController {
         $0.backgroundColor = .black
         $0.alpha = 0.0
     }
+    private let titleContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
     private let titleLabel = UILabel().then {
-        $0.text = "목표기간"
+        $0.text = "반복 주기"
         $0.textColor = .white
         $0.font = .semiBold(with: 20)
     }
-    private let calendarView = CalendarView()
-    private let pickerView = UIPickerView()
+    private let repeatDateStackView = RepeatDateStackView()
+    private let repeatWeekStackView = RepeatWeekdayView().then {
+        $0.isHidden = true
+    }
+    private let dayCountContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
     private let dayCountLabel = UILabel().then {
         $0.text = "-"
         $0.textColor = .green
         $0.font = .semiBold(with: 20)
     }
-    private let completedButton = UIButton().then {
-        $0.setTitle("완료", for: .normal)
-        $0.setTitle("완료", for: .highlighted)
-        $0.setTitleColor(.lightGray, for: .normal)
-        $0.setTitleColor(.lightGray, for: .highlighted)
-        $0.setTitleColor(.black, for: .selected)
+    private let buttonContainerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.setTitle("취소", for: .highlighted)
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitleColor(.white, for: .highlighted)
         $0.titleLabel?.font = .semiBold(with: 18)
         $0.layer.cornerRadius = 27.0
         $0.backgroundColor = .default
     }
+    private let completedButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitle("완료", for: .highlighted)
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitleColor(.black, for: .highlighted)
+        $0.titleLabel?.font = .semiBold(with: 18)
+        $0.layer.cornerRadius = 27.0
+        $0.backgroundColor = .green
+    }
     
     //MARK: - Life Cycle
-    init(startDate: Date? = nil, endDate: Date? = nil) {
+    init(type: RepeatType, startDate: Date? = nil, endDate: Date? = nil) {
+        self.repeatType = type
         self.startDate = startDate
         self.endDate = endDate
         
@@ -80,51 +109,65 @@ final class RepeatVC: UIViewController {
     private func makeUI() {
         view.backgroundColor = .clear
         
+        let width = UIScreen.main.bounds.width
+        let btnWidth = (width - 39) / 2
+        
         view.addSubview(visualEffectView)
         view.addSubview(containerView)
-        containerView.addSubview(titleLabel)
-        containerView.addSubview(calendarView)
-        containerView.addSubview(pickerContainerView)
-        containerView.addSubview(dayCountLabel)
-        containerView.addSubview(completedButton)
-        pickerContainerView.addSubview(pickerView)
+        containerView.addArrangedSubview(titleContainerView)
+        containerView.addArrangedSubview(repeatDateStackView)
+        containerView.addArrangedSubview(repeatWeekStackView)
+        containerView.addArrangedSubview(pickerContainerView)
+        containerView.addArrangedSubview(dayCountContainerView)
+        containerView.addArrangedSubview(buttonContainerView)
+        
+        titleContainerView.addSubview(titleLabel)
+        dayCountContainerView.addSubview(dayCountLabel)
+        buttonContainerView.addSubview(completedButton)
+        buttonContainerView.addSubview(cancelButton)
         
         visualEffectView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         containerView.snp.makeConstraints {
             $0.left.right.equalToSuperview()
-            $0.bottom.equalToSuperview().offset(37)
-            $0.height.equalTo(625)
+            $0.bottom.equalToSuperview()
         }
-        completedButton.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(16)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
-            $0.height.equalTo(54)
-        }
-        dayCountLabel.snp.makeConstraints {
-            $0.left.equalToSuperview().offset(24)
-            $0.bottom.equalTo(completedButton.snp.top).offset(-32)
-            $0.height.equalTo(30)
-        }
-        calendarView.snp.makeConstraints {
-            $0.bottom.equalTo(dayCountLabel.snp.top).offset(-17)
-            $0.horizontalEdges.equalToSuperview().inset(20)
-            $0.height.equalTo(377)
-        }
-        pickerContainerView.snp.makeConstraints {
-            $0.left.right.bottom.equalTo(calendarView)
-            $0.height.equalTo(321)
-        }
-        pickerView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.equalTo(200)
-            $0.height.equalTo(238)
+        titleContainerView.snp.makeConstraints {
+            $0.height.equalTo(68)
         }
         titleLabel.snp.makeConstraints {
-            $0.bottom.equalTo(calendarView.snp.top)
             $0.left.equalToSuperview().offset(20)
+            $0.right.bottom.equalToSuperview()
             $0.height.equalTo(44)
+        }
+        repeatDateStackView.snp.makeConstraints {
+            $0.height.equalTo(36)
+        }
+        repeatWeekStackView.snp.makeConstraints {
+            $0.height.equalTo(40)
+        }
+        dayCountContainerView.snp.makeConstraints {
+            $0.height.equalTo(30)
+        }
+        dayCountLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(20)
+            $0.top.right.bottom.equalToSuperview()
+        }
+        buttonContainerView.snp.makeConstraints {
+            $0.height.equalTo(80)
+        }
+        cancelButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.equalTo(btnWidth)
+            $0.height.equalTo(54)
+        }
+        completedButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.right.equalToSuperview().offset(-16)
+            $0.width.equalTo(btnWidth)
+            $0.height.equalTo(54)
         }
     }
     
@@ -141,131 +184,98 @@ final class RepeatVC: UIViewController {
         completedButton
             .tapPublisher
             .sink { [weak self] _ in
-                guard let self, self.completedButton.isSelected else {
-                    let alert = UIAlertController(title: "알림", message: "기간을 선택해주세요.", preferredStyle: .alert)
-                    let action = UIAlertAction(title: "확인", style: .default)
-                    alert.addAction(action)
-                    self?.present(alert, animated: true)
-                    return
+                guard let self else { return }
+                
+                var result = Set<Int>()
+                switch self.repeatType {
+                case .everyday:
+                    (1...7).forEach { result.insert($0) }
+                    self.delegate?.select(days: result)
+                case .weekday:
+                    (2...6).forEach { result.insert($0) }
+                    self.delegate?.select(days: result)
+                case .weekend:
+                    result.insert(1)
+                    result.insert(7)
+                    self.delegate?.select(days: result)
+                case .custom(let days):
+                    let dayStrings = ["일", "월", "화", "수", "목", "금", "토"]
+                    self.delegate?.select(days: days)
                 }
-                self.delegate?.select(startDate: self.startDate, endDate: self.endDate)
+                
                 self.dismiss(animated: true)
+            }
+            .store(in: &bag)
+        
+        cancelButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                self?.dismiss(animated: true)
+            }
+            .store(in: &bag)
+        
+        $repeatType
+            .sink { [weak self] type in
+                guard let self, let startDate = self.startDate, let endDate = self.endDate else { return }
+                var totalDayCount = 0
+                var sDay = startDate
+                self.repeatWeekStackView.isHidden = true
+                
+                switch type {
+                case .everyday:
+                    let result = endDate.timeIntervalSince1970 - startDate.timeIntervalSince1970
+                    totalDayCount = Int(result / 60 / 60 / 24) + 1
+                case .weekday:
+                    while sDay <= endDate {
+                        let isWeekday = (2...6) ~= self.calendar.component(.weekday, from: sDay)
+                        if isWeekday { totalDayCount += 1 }
+                        sDay.addTimeInterval(86400)
+                    }
+                case .weekend:
+                    while sDay <= endDate {
+                        let weekday = self.calendar.component(.weekday, from: sDay)
+                        let isWeekday = 1 == weekday || 7 == weekday
+                        if isWeekday { totalDayCount += 1 }
+                        sDay.addTimeInterval(86400)
+                    }
+                case .custom(let days):
+                    self.repeatWeekStackView.isHidden = false
+                    while sDay <= endDate {
+                        let weekday = self.calendar.component(.weekday, from: sDay)
+                        if days.contains(weekday) { totalDayCount += 1 }
+                        sDay.addTimeInterval(86400)
+                    }
+                }
+                
+                self.dayCountLabel.text = "총 \(totalDayCount)일"
             }
             .store(in: &bag)
     }
     
     func configure() {
-        calendarView.delegate = self
-        calendarView.configure(with: self.startDate ?? Date(), startDate: self.startDate, endDate: self.endDate)
+        repeatDateStackView.delegate = self
+        repeatDateStackView.configure(type: self.repeatType)
         
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        pickerView.backgroundColor = .black
-        
-        let year = calendar.component(.year, from: calendarView.targetDate ?? Date()) - 2000
-        let month = calendar.component(.month, from: calendarView.targetDate ?? Date()) - 1
-        pickerView.selectRow(year, inComponent: 0, animated: false)
-        pickerView.selectRow(month, inComponent: 1, animated: false)
-        
-        let isSelected = startDate != nil && endDate != nil
-        completedButton.isSelected = isSelected
-        completedButton.backgroundColor = isSelected ? .green : .default
-    }
-    
-    private func isDate(lhs: Date?, rhs: Date?) -> Bool {
-        guard let lhs, let rhs else { return false }
-        
-        let lhsDateString = lhs.dateString
-        let rhsDateString = rhs.dateString
-        
-        return lhsDateString == rhsDateString
-    }
-}
-
-extension RepeatVC: UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return component == 0 ? 101 : 12
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let text = component == 0 ? "\(years[row])년" : "\(month[row])월"
-        let lbl = UILabel()
-        lbl.text = text
-        lbl.textColor = .white
-        lbl.textAlignment = .center
-        lbl.font = .semiBold(with: 22)
-        return lbl
-    }
-}
-
-extension RepeatVC: UIPickerViewDelegate {
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        guard let date = calendarView.targetDate else { return }
-        
-        var components = DateComponents()
-        
-        let year = years[pickerView.selectedRow(inComponent: 0)]
-        let month = month[pickerView.selectedRow(inComponent: 1)]
-        
-        components.year = year
-        components.month = month
-        components.day = 1
-        
-        if year != calendar.component(.year, from: date) || month != calendar.component(.month, from: date) {
-            self.calendarView.isEnd = true
-            self.calendarView.targetDate = calendar.date(from: components)
-        }
-        
-        if year != calendar.component(.year, from: date) || month != calendar.component(.month, from: date) {
-            self.calendarView.isEnd = true
-            self.calendarView.targetDate = calendar.date(from: components)
+        repeatWeekStackView.delegate = self
+        if case let .custom(days) = self.repeatType {
+            self.days = days
+            repeatWeekStackView.isHidden = false
+            repeatWeekStackView.configure(days: days)
+        } else {
+            repeatWeekStackView.isHidden = true
+            repeatWeekStackView.configure(days: [])
         }
     }
 }
 
-extension RepeatVC: CalendarViewDelegate {
-    func select(title: Date?) {
-        guard let date = title else { return }
-        
-        let isOpen = self.pickerContainerView.alpha == .zero
-        
-        let year = calendar.component(.year, from: date) - 2000
-        let month = calendar.component(.month, from: date) - 1
-        pickerView.selectRow(year, inComponent: 0, animated: false)
-        pickerView.selectRow(month, inComponent: 1, animated: false)
-        
-        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .easeInOut)
-        let angle: CGFloat = isOpen ? .pi : -.pi * 2
-        let tr = CGAffineTransform.identity.rotated(by: angle)
-        
-        animator.addAnimations { [weak self] in
-            self?.pickerContainerView.alpha = isOpen ? 1.0 : .zero
-            self?.calendarView.titleImageView.transform = tr
-        }
-        
-        animator.startAnimation()
+extension RepeatPopupVC: RepeatDateStackViewDelegate {
+    func select(repeatType: RepeatType) {
+        self.repeatType = repeatType
     }
-    
-    func select(startDate: Date?, endDate: Date?) {
-        guard let startDate, let endDate else {
-            self.dayCountLabel.text = "-"
-            self.completedButton.isSelected = false
-            self.completedButton.backgroundColor = .default
-            return
-        }
-        self.startDate = startDate
-        self.endDate = endDate
-        
-        self.completedButton.isSelected = true
-        self.completedButton.backgroundColor = .green
-        
-        let reulst = endDate.timeIntervalSince1970 - startDate.timeIntervalSince1970
-        let day = Int(reulst / 60 / 60 / 24) + 1
-        
-        self.dayCountLabel.text = "총 \(day)일"
+}
+
+extension RepeatPopupVC: RepeatWeekdayViewDelegate {
+    func select(days: Set<Int>) {
+        repeatType = .custom(days)
     }
 }
--- LiveGodLife/LiveGodLife/Scene/Todo/Create/Popuo/View/RepeatWeekdayView.swift
+++ LiveGodLife/LiveGodLife/Scene/Todo/Create/Popuo/View/RepeatWeekdayView.swift
@@ -5,4 +5,238 @@
 //  Created by wargi on 2023/02/26.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+import Combine
+import CombineCocoa
+
+protocol RepeatWeekdayViewDelegate: AnyObject {
+    func select(days: Set<Int>)
+}
+
+//MARK: RepeatDateStackView
+final class RepeatWeekdayView: UIView {
+    //MARK: - Properties
+    @Published private var days = Set<Int>()
+    weak var delegate: RepeatWeekdayViewDelegate?
+    private var bag = Set<AnyCancellable>()
+    private let stackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 8
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+    }
+    private let sunButton = UIButton().then {
+        $0.setTitle("일", for: .normal)
+        $0.setTitle("일", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 1
+    }
+    private let monButton = UIButton().then {
+        $0.setTitle("월", for: .normal)
+        $0.setTitle("월", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 2
+    }
+    private let tueButton = UIButton().then {
+        $0.setTitle("화", for: .normal)
+        $0.setTitle("화", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 3
+    }
+    private let wedButton = UIButton().then {
+        $0.setTitle("수", for: .normal)
+        $0.setTitle("수", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 4
+    }
+    private let thuButton = UIButton().then {
+        $0.setTitle("목", for: .normal)
+        $0.setTitle("목", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 5
+    }
+    private let friButton = UIButton().then {
+        $0.setTitle("금", for: .normal)
+        $0.setTitle("금", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 6
+    }
+    private let satButton = UIButton().then {
+        $0.setTitle("토", for: .normal)
+        $0.setTitle("토", for: .highlighted)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .normal)
+        $0.setTitleColor(UIColor(rgbHexString: "#8D8D93"), for: .highlighted)
+        $0.setTitleColor(.white, for: .selected)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.backgroundColor = .black
+        $0.titleLabel?.font = .regular(with: 14)
+        $0.tag = 7
+    }
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        makeUI()
+        bind()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+
+        addSubview(stackView)
+        stackView.addArrangedSubview(sunButton)
+        stackView.addArrangedSubview(monButton)
+        stackView.addArrangedSubview(tueButton)
+        stackView.addArrangedSubview(wedButton)
+        stackView.addArrangedSubview(thuButton)
+        stackView.addArrangedSubview(friButton)
+        stackView.addArrangedSubview(satButton)
+        
+        stackView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(16)
+            $0.top.equalToSuperview()
+            $0.width.equalTo(328)
+            $0.height.equalTo(40)
+        }
+        sunButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        monButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        tueButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        wedButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        thuButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        friButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+        satButton.snp.makeConstraints {
+            $0.size.equalTo(40)
+        }
+    }
+    
+    private func bind() {
+        monButton
+            .tapPublisher
+            .map { self.monButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        tueButton
+            .tapPublisher
+            .map { self.tueButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        wedButton
+            .tapPublisher
+            .map { self.wedButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        thuButton
+            .tapPublisher
+            .map { self.thuButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        friButton
+            .tapPublisher
+            .map { self.friButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        satButton
+            .tapPublisher
+            .map { self.satButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+        
+        sunButton
+            .tapPublisher
+            .map { self.sunButton }
+            .sink { self.update(sender: $0) }
+            .store(in: &bag)
+    }
+    
+    func configure(days: Set<Int>) {
+        days.forEach {
+            if let btn = self.stackView.viewWithTag($0) as? UIButton {
+                update(sender: btn)
+            }
+        }
+    }
+    
+    func update(sender: UIButton) {
+        if self.days.contains(sender.tag) {
+            self.days.remove(sender.tag)
+            sender.isSelected = false
+            sender.layer.borderColor = UIColor(rgbHexString: "#39393D")?.cgColor
+            sender.titleLabel?.font = .regular(with: 14)
+        } else {
+            self.days.insert(sender.tag)
+            sender.isSelected = true
+            sender.layer.borderColor = UIColor.green.cgColor
+            sender.titleLabel?.font = .semiBold(with: 14)
+        }
+        
+        self.delegate?.select(days: self.days)
+    }
+}
