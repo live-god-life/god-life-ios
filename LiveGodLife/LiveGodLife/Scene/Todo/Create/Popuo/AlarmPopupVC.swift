5153b677486ff38064628a3daf064f38f2884c15
wargi <wkahdla12346@gmail.com>
2023-02-17 21:59:03 +0900

    feature: 알람 팝업 구현(1)

---
+++ LiveGodLife/LiveGodLife/Scene/Todo/Create/Popuo/AlarmPopupVC.swift
@@ -0,0 +1,226 @@
+//
+//  AlarmPopupVC.swift
+//  LiveGodLife
+//
+//  Created by wargi on 2023/02/27.
+//
+
+import Then
+import SnapKit
+import UIKit
+import Combine
+
+protocol AlarmPopupVCDelegate: AnyObject {
+    func select(time: Date?)
+}
+
+//MARK: DatePopupVC
+final class AlarmPopupVC: UIViewController {
+    //MARK: - Properties
+    private var date: Date?
+    private var ampm = ["오전", "오후"]
+    private var hours = (1...12).map { $0 }
+    private var minutes = (0...59).map { $0 }
+    weak var delegate: AlarmPopupVCDelegate?
+    private var bag = Set<AnyCancellable>()
+    private let calendar = Calendar.current
+    private let visualEffectView = CustomVisualEffectView()
+    private let datePicker = UIPickerView()
+    private let containerView = UIView().then {
+        $0.backgroundColor = .black
+    }
+    private let titleLabel = UILabel().then {
+        $0.text = "알람설정"
+        $0.textColor = .white
+        $0.font = .semiBold(with: 20)
+    }
+    private let notUsedLabel = UILabel().then {
+        $0.text = "사용안함"
+        $0.font = .semiBold(with: 16)
+    }
+    private let notUsedImageView = UIImageView().then {
+        $0.image = UIImage(named: "empty-checkbox")
+    }
+    private let notUsedButton = UIButton()
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.setTitle("취소", for: .highlighted)
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitleColor(.white, for: .highlighted)
+        $0.titleLabel?.font = .semiBold(with: 18)
+        $0.layer.cornerRadius = 27.0
+        $0.backgroundColor = .default
+    }
+    private let completedButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitle("완료", for: .highlighted)
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitleColor(.black, for: .highlighted)
+        $0.titleLabel?.font = .semiBold(with: 18)
+        $0.layer.cornerRadius = 27.0
+        $0.backgroundColor = .green
+    }
+    
+    //MARK: - Life Cycle
+    init(time: Date?) {
+        self.date = time
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeUI()
+        bind()
+        configure()
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        view.backgroundColor = .clear
+        
+        let width = UIScreen.main.bounds.width
+        let btnWidth = (width - 39) / 2
+        
+        view.addSubview(visualEffectView)
+        view.addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(notUsedImageView)
+        containerView.addSubview(notUsedLabel)
+        containerView.addSubview(notUsedButton)
+        containerView.addSubview(cancelButton)
+        containerView.addSubview(completedButton)
+        containerView.addSubview(datePicker)
+        
+        visualEffectView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        containerView.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-37)
+            $0.height.equalTo(396)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(24)
+            $0.left.equalToSuperview().offset(20)
+            $0.height.equalTo(44)
+        }
+        notUsedImageView.snp.makeConstraints {
+            $0.right.equalToSuperview().offset(-16)
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.size.equalTo(24)
+        }
+        notUsedLabel.snp.makeConstraints {
+            $0.right.equalTo(notUsedImageView.snp.left).offset(-8)
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+        }
+        notUsedButton.snp.makeConstraints {
+            $0.left.equalTo(notUsedLabel.snp.left)
+            $0.right.equalTo(notUsedImageView.snp.right)
+            $0.top.bottom.equalTo(titleLabel)
+        }
+        cancelButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.equalTo(btnWidth)
+            $0.height.equalTo(54)
+        }
+        completedButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.right.equalToSuperview().offset(-16)
+            $0.width.equalTo(btnWidth)
+            $0.height.equalTo(54)
+        }
+        datePicker.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(24)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalTo(cancelButton.snp.top).offset(-24)
+        }
+    }
+    
+    //MARK: - Binding..
+    private func bind() {
+        visualEffectView
+            .backgroundButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                self?.dismiss(animated: true)
+            }
+            .store(in: &bag)
+        
+        completedButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                guard let self else { return }
+                
+                self.delegate?.select(time: self.notUsedButton.isSelected ? nil : self.date)
+                
+                self.dismiss(animated: true)
+            }
+            .store(in: &bag)
+        
+        cancelButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                self?.dismiss(animated: true)
+            }
+            .store(in: &bag)
+        
+        notUsedButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                let isSelected = !(self?.notUsedButton.isSelected ?? false)
+                self?.notUsedButton.isSelected = isSelected
+                
+                self?.notUsedImageView.image = isSelected ? UIImage(named: "fill-checkbox") : UIImage(named: "empty-checkbox")
+            }
+            .store(in: &bag)
+        
+    }
+    
+    func configure() {
+        datePicker.delegate = self
+        datePicker.dataSource = self
+    }
+}
+
+extension AlarmPopupVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == 0 ? 2 : component == 1 ? 12 : 60
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let text = component == 0 ? ampm[row] : component == 1 ? "\(hours[row])" : "\(minutes[row])"
+        let lbl = UILabel()
+        lbl.text = text
+        lbl.textColor = .white
+        lbl.textAlignment = .center
+        lbl.font = .semiBold(with: 22)
+        return lbl
+    }
+}
+
+extension AlarmPopupVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var components = DateComponents()
+        
+        let pm = ampm[pickerView.selectedRow(inComponent: 0)] == "오후" ? 12 : .zero
+        let hour = hours[pickerView.selectedRow(inComponent: 1)] + pm
+        let min = minutes[pickerView.selectedRow(inComponent: 2)]
+        
+        components.hour = hour == 24 ? 0 : hour
+        components.minute = min
+        components.day = 1
+        
+        self.date = calendar.date(from: components)
+    }
+}
